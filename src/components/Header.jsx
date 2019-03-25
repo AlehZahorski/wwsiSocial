8656f1e4aa762a198cb979a20d2fca046eef9ebb
AlehZahorski <git3371m@gmail.com>
2019-03-25 20:21:31 +0100

    Made some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './images/domain.svg';
+import '../App.css';
 
 
 const Header = () => {
@@ -11,9 +12,21 @@ return (
   </div>
 
 
-     <div className="topmenuQ">
-        DropMenu
-     </div>
+
+
+    <div id="sidebar">
+    <div className="toggle-btn" onclick="openMenu()">
+    <span></span>
+    <span></span>
+    <span></span>
+    </div>
+      <ul>
+        <li><a href="#">Вход</a></li>
+        <li><a href="#">Выход</a></li>
+        <li><a href="#">Регистрация</a></li>
+      </ul>
+    </div>
+
 
 
 </header>
