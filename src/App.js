eb50699088e10e8e6c104fdc69d4758ae831e662
AlehZahorski <git3371m@gmail.com>
2019-04-08 21:48:21 +0200

    qqq

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Footer from './components/Footer/Footer';
 import Dialogs from './components/Dialogs/Dialogs.jsx';
+import DropDown from './components/Header/DropDown/DropDown.jsx';
+
 const App = (props) => {
     return (
 
         <BrowserRouter>
             <switch className="app-wrapper">
-                <Header/>
-                <Navbar/>
+                <Header />
+                <Navbar />
                 <div className="app-wrapper-content">
                     <Route exact path="/Profile" component={Profile}/>
                     <Route exact path="/Dialogs" component={Dialogs}/>
