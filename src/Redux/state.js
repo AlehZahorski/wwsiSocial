d62ca741cd5a82ed09d66041f27e1ee3f4aba784
AlehZahorski <git3371m@gmail.com>
2019-05-04 12:10:19 +0200

    made this and store

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,51 +1,57 @@
-let rerenderFullApp = () => {
-  console.log('Hello, we have changes in state!');
-}
-
-let state = {
-  ProfilePage: {
-   posts: [
-  { message: 'Tomorrow i have work on google', LikesCount:41},
-  { message: 'Yesterday i started learning React', LikesCount:45}
-],
-   NewPostText: 'Enter your message..'
-},
-  DialogsPage: {
-   dialogs: [
-  { id: 1, name: 'Andzrej'},
-  { id: 2, name: 'Grzegorz'},
-  { id: 3, name: 'Swetlana'},
-  { id: 4, name: 'Dmitrij'},
-  { id: 5, name: 'Konstantin'}
-],
-   messages:  [
-  { id: 1, message: 'Hi. whatsup????'},
-  { id: 2, message: 'Hello. How are you?'},
-  { id: 3, message: 'elo elo elo!'},
-  { id: 4, message: 'Эй, где мои бабки.. чувак?!!'},
-  { id: 5, message: 'Привет!'}
-]
-}
-}
-
-window.state = state;
-
-export const addPost = () => {
-  let newPost ={
-    id: 5,
-    message: state.ProfilePage.NewPostText,
-    LikesCount: 0
-  };
-  state.ProfilePage.posts.push(newPost);
-  rerenderFullApp(state);
-}
-
-export const updateNewPostText = (newText) => {
-  state.ProfilePage.NewPostText = newText;
-  rerenderFullApp(state);
+let store = {
+  _state: {
+    ProfilePage: {
+      posts: [
+        { message: 'Tomorrow i have work on google', LikesCount:41},
+        { message: 'Yesterday i started learning React', LikesCount:45}
+      ],
+      NewPostText: 'Enter your message..'
+    },
+    DialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Andzrej'},
+        { id: 2, name: 'Grzegorz'},
+        { id: 3, name: 'Swetlana'},
+        { id: 4, name: 'Dmitrij'},
+        { id: 5, name: 'Konstantin'}
+      ],
+      messages:  [
+        { id: 1, message: 'Hi. whatsup????'},
+        { id: 2, message: 'Hello. How are you?'},
+        { id: 3, message: 'elo elo elo!'},
+        { id: 4, message: 'Эй, где мои бабки.. чувак?!!'},
+        { id: 5, message: 'Привет!'}
+      ]
+    }
+  },
+  getState(){
+    return this._state;
+  },
+  rerenderFullApp(){
+    console.log('Hello, we have changes in state!');
+  },
+  _callSubscriber(){
+    console.log('hello');
+  },
+  addPost(){
+    debugger;
+    let newPost ={
+      id: 5,
+      message: this._state.ProfilePage.NewPostText,
+      LikesCount: 0
+    };
+    this._state.ProfilePage.posts.push(newPost);
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText){
+    this._state.ProfilePage.NewPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer){
+    this._callSubscriber = observer;
   }
+}
 
-export const subscribe = (observer) => {
-rerenderFullApp = observer;
-  }
-export default state;
+
+window.store = store;
+export default store;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, updateNewPostText} from "./Redux/state.js";
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
-import state, {subscribe} from './Redux/state';
+import store from './Redux/state';
 
 
 
 let rerenderFullApp = (state) => {
 ReactDOM.render(
   <BrowserRouter>
-  <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+  <App state={state} addPost={store.addPost.bind(store)}
+   updateNewPostText={store.updateNewPostText.bind(store)}/>
   </BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderFullApp(state);
+rerenderFullApp(store.getState());
 
-subscribe(rerenderFullApp);
+store.subscribe(rerenderFullApp);
 
 
 // If you want your app to work offline and load faster, you can change
